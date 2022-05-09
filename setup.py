1ba80d9105e688732d6546a08f9df98f4bb3e5e8
Us_man <100738404+Namsu88@users.noreply.github.com>
2022-05-09 20:53:20 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+setuptools.setup (
+  include_package_data = True,
+  name='mycalculatator001',
+  version='0.0.1',
+  description='oss-dev my calculator 001',
+  author='namsuheo',
+  author_email='sinaie99@naver.com',
+  url = "",
+  download_url = "",
+  install_requires=['pytest'],
+  long_description = 'oss-dev calculator python module',
+  long_description_content_type = 'text/markdown',
+  classifiers=[
+    "Programming Language :: Python :: 3",
+    "Operating System :: OS Independent",
+  ],
+)
